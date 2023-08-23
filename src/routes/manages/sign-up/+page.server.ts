1aa41e7f84993e6a42f6edf925fe8622346884d6
Chloe <gtalk.with.nani@gmail.com>
2023-08-23 20:59:09 +0900

    Validated Sign-up error handling logic

--- src/routes/manages/sign-up/+page.server.ts
+++ src/routes/manages/sign-up/+page.server.ts
@@ -1,7 +1,15 @@
-import { fail, redirect } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
 import prisma from '$lib/prisma';
 import bcrypt from 'bcrypt';
 import type { Actions } from './$types';
+
+type Errors = {
+  userid?: string;
+  username?: string;
+  email?: string;
+  password?: string;
+  passwordConfirmation?: string;
+};
 
 const validateEmail = (email: string) => {
   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
@@ -11,43 +19,40 @@ export const actions = {
   default: async ({ request }) => {
     const data = await request.formData();
 
-    const userid = data.get('userid');
-    const username = data.get('username');
-    const email = data.get('email');
-    const password = data.get('password');
-    const passwordConfirmation = data.get('passwordConfirmation');
+    const userid = data.get('userid') as string;
+    const username = data.get('username') as string;
+    const email = data.get('email') as string;
+    const password = data.get('password') as string;
+    const passwordConfirmation = data.get('passwordConfirmation') as string;
 
-    if (!username || !email || !userid || !password || !passwordConfirmation) {
-      return fail(400, { missing: true });
-    }
-
-    if (
-      typeof username !== 'string' ||
-      typeof email !== 'string' ||
-      typeof userid !== 'string' ||
-      typeof password !== 'string' ||
-      typeof passwordConfirmation !== 'string'
-    ) {
-      return fail(400, { incorrect: true });
-    }
-
-    if (password !== passwordConfirmation) {
-      return fail(400, { passwordMismatch: true });
-    }
-
-    if (!validateEmail(email)) {
-      return fail(400, { incorrect: true });
-    }
-
-    const existingEmailUser = await prisma.user.findUnique({ where: { email: email } });
-    if (existingEmailUser) {
-      return fail(400, { emailExists: true });
-    }
+    const errors: Partial<Errors> = {};
 
     const existingUserId = await prisma.user.findUnique({ where: { userid } });
-    if (existingUserId) {
-      return fail(400, { useridExists: true });
-    }
+    if (!userid) errors['userid'] = '아이디는 필수 항목입니다';
+    else if (userid.length < 4) errors['userid'] = '아이디는 4자 이상 입력해 주세요';
+    else if (existingUserId) errors['userid'] = '이미 존재하는 아이디입니다';
+    else delete errors['userid'];
+
+    if (!username) errors['username'] = '이름은 필수 항목입니다';
+    else if (username.length < 1) errors['username'] = '이름은 2자 이상 입력해주세요';
+    else delete errors['username'];
+
+    const existingEmailUser = await prisma.user.findUnique({ where: { email: email } });
+    if (!email) errors['email'] = '이메일은 필수 항목입니다';
+    else if (!validateEmail(email)) errors['email'] = '이메일 형식이 맞지 않습니다';
+    else if (existingEmailUser) errors['email'] = '이미 존재하는 이메일입니다';
+    else delete errors['email'];
+
+    if (password.length < 10) errors['password'] = '비밀번호는 10자 이상 입력해 주세요';
+    else delete errors['password'];
+
+    if (password !== passwordConfirmation)
+      errors['passwordConfirmation'] = '비밀번호가 일치하지 않습니다';
+    else delete errors['passwordConfirmation'];
+
+    console.log('Object.keys(errors).length):', Object.keys(errors).length);
+
+    if (Object.keys(errors).length) return fail(400, { errors });
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
--- src/routes/manages/sign-up/+page.svelte
+++ src/routes/manages/sign-up/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import { toast } from 'svelte-french-toast';
+  import toast, { Toaster } from 'svelte-french-toast';
   import { applyAction, enhance } from '$app/forms';
   import type { ActionData } from './$types';
   import { goto } from '$app/navigation';
   export let form: ActionData;
 </script>
+
+<Toaster />
 
 <main class="container">
   <div class="content">
@@ -15,11 +17,12 @@
           await applyAction(result);
           if (result.type === 'success') {
             const user = result.data?.user;
-            toast('회원가입에 성공했습니다.', {
-              duration: 2000,
+            await goto('/manages/pass');
+          } else {
+            toast.error('잘못 입력된 항목이 있습니다.', {
+              duration: 5000,
               position: 'top-center'
             });
-            await goto('/manages/pass');
           }
         }}
     >
@@ -27,20 +30,32 @@
         <legend>회원가입 양식</legend>
         <div class="form-group">
           <div class="field-group">
-            <input id="userid" name="userid" placeholder="아이디" />
+            <input type="text" id="userid" name="userid" placeholder="아이디" required />
             <label for="userid">아이디</label>
+            {#if form?.errors?.userid}
+              <p>{form.errors.userid}</p>
+            {/if}
           </div>
           <div class="field-group">
-            <input id="username" name="username" placeholder="사용자 이름" />
+            <input type="text" id="username" name="username" placeholder="사용자 이름" required />
             <label for="username">사용자 이름</label>
+            {#if form?.errors?.username}
+              <p>{form.errors.username}</p>
+            {/if}
           </div>
           <div class="field-group">
-            <input type="email" id="email" name="email" placeholder="이메일 주소" />
+            <input type="email" id="email" name="email" placeholder="이메일 주소" required />
             <label for="email">이메일 주소</label>
+            {#if form?.errors?.email}
+              <p>{form.errors.email}</p>
+            {/if}
           </div>
           <div class="field-group">
-            <input type="password" id="password" name="password" placeholder="비밀번호" />
+            <input type="password" id="password" name="password" placeholder="비밀번호" required />
             <label for="password">비밀번호</label>
+            {#if form?.errors?.password}
+              <p>{form.errors.password}</p>
+            {/if}
           </div>
           <div class="field-group">
             <input
@@ -48,8 +63,12 @@
               id="passwordConfirmation"
               name="passwordConfirmation"
               placeholder="비밀번호 확인"
+              required
             />
             <label for="passwordConfirmation">비밀번호 확인</label>
+            {#if form?.errors?.passwordConfirmation}
+              <p>{form.errors.passwordConfirmation}</p>
+            {/if}
           </div>
         </div>
         <div class="button-group">
