9f0ec0777a80ac8905d26f870e540d319e6250fc
Chloe <gtalk.with.nani@gmail.com>
2023-08-24 17:11:11 +0900

    Sign-in Available

---
+++ src/routes/manages/pass/+page.server.ts
@@ -0,0 +1,34 @@
+import { prisma } from '$lib/prisma';
+import { auth } from '$lib/auth';
+import { fail } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import bcrypt from 'bcryptjs';
+
+export const actions: Actions = {
+  async default(event) {
+    const data = await event.request.formData();
+    const userid = data.get('userid') as string;
+    const password = data.get('password') as string;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        userid: userid
+      }
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      const error = '계정이 없거나 ID/비밀번호가 잘못되었습니다';
+      return fail(401, { userid, error });
+    }
+
+    const { token } = await auth(userid);
+
+    event.cookies.set('AuthorizationToken', `Bearer ${token}`, {
+      httpOnly: true,
+      path: '/',
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 14
+    });
+  }
+} satisfies Actions;
---
+++ src/routes/manages/pass/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import toast, { Toaster } from 'svelte-french-toast';
+  import { applyAction, enhance } from '$app/forms';
+  import type { ActionData } from './$types';
+  import { goto } from '$app/navigation';
+  export let form: ActionData;
+</script>
+
+<Toaster />
+
+<main class="container">
+  <div class="content">
+    <form
+      method="POST"
+      use:enhance={() =>
+        async ({ result }) => {
+          await applyAction(result);
+          if (result.type === 'success') {
+            const user = result.data?.user;
+            await goto('/manages/settings');
+          } else {
+            toast.error('계정이 없거나 ID/비밀번호가 잘못되었습니다', {
+              duration: 5000,
+              position: 'top-center'
+            });
+          }
+        }}
+    >
+      <fieldset>
+        <legend>로그인 양식</legend>
+        <div class="form-group">
+          <div class="field-group">
+            <input type="text" id="userid" name="userid" placeholder="아이디" required />
+            <label for="userid">아이디</label>
+          </div>
+          <div class="field-group">
+            <input type="password" id="password" name="password" placeholder="비밀번호" required />
+            <label for="password">비밀번호</label>
+          </div>
+        </div>
+        <div class="button-group">
+          <button type="submit">로그인하기</button>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</main>
