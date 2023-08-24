fe87b071d93ad01ffec801322d070dd24fcb6258
Chloe <gtalk.with.nani@gmail.com>
2023-08-24 17:09:28 +0900

    Change Schema

--- authKeyGenerator.js
+++ authKeyGenerator.js
@@ -1,8 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import crypto from 'crypto';
-
-const tokenKey = uuidv4();
-console.log(`VITE_PUBLIC_TOKEN=${tokenKey}`);
-
 const jwtSecretKey = crypto.randomBytes(64).toString('hex');
-console.log(`VITE_PUBLIC_JWT_SECRET=${jwtSecretKey}`);
+console.log(`JWT_ACCESS_SECRET=${jwtSecretKey}`);
